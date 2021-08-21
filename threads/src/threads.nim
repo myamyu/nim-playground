0cbb6adeb3916f338728e72ebf00940c34350358
みゃみゅ玉子 <myamyu@gmail.com>
2021-08-21 15:58:17 +0000

    はじめとおわり

--- threads/src/threads.nim
+++ threads/src/threads.nim
@@ -1,9 +1,11 @@
 import random
 
 proc hoge(args:tuple[a:int, name:string]) {.thread.} =
+  echo args.name, ": はじめるよー。 ", $args.a, " tasks."
   for i in 0..args.a:
     let p = i.float / args.a.float * 100.0
     echo args.name, ": ", p, "%"
+  echo args.name, ": おわったよー。"
 
 when isMainModule:
   var ths:array[10, Thread[tuple[a:int, name:string]]]
