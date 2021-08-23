f50ca20d15b3fb1dcb2bc838f3f0a7521811a686
みゃみゅ玉子 <myamyu@gmail.com>
2021-08-23 16:14:03 +0000

    loggingがあった

---
+++ logloglog/src/logloglog.nim
@@ -0,0 +1,18 @@
+import logging
+
+const fmtStr = "$date $time - [$levelname] "
+let logger1 = newConsoleLogger(fmtStr=fmtStr)
+let logger2 = newFileLogger(filename="./logs/app.log", fmtStr=fmtStr)
+let logger3 = newRollingFileLogger(filename="./logs/app-rolling.log", fmtStr=fmtStr)
+
+addHandler(logger1)
+addHandler(logger2)
+addHandler(logger3)
+
+when isMainModule:
+  debug("でばっぐだよ")
+  info("いんふぉだよ")
+  notice("noticeだよ")
+  warn("けいこくだよ")
+  error("えらーだよ")
+  fatal("やばいよやばいよ")
