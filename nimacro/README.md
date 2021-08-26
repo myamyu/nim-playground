32019134a0c4d1b995a58d6856b6d3811e4b34a3
みゃみゅ玉子 <myamyu@gmail.com>
2021-08-26 16:38:52 +0000

    それっぽいmacroできた

--- nimacro/README.md
+++ nimacro/README.md
@@ -1,7 +1,27 @@
 # nimのmacroにふれる
 
-理解できるか・・・macroを・・・
+雑なHTMLマクロを作った。
+
+- Elementとテキストしか対応してない
+- ループを組み込んだりできない
+
+```nim
+html:
+  head:
+    title "たいとる"
+  body:
+    h1 "たいとるだよ"
+    main:
+      section:
+        h2 "せくしょん"
+        p:
+          "いえっさー"
+          br()
+          "おいっさー"
+```
 
 ## 参考
 
 - https://qiita.com/MtShiba/items/b06fff4c4ef98769c781
+- https://flat-leon.hatenablog.com/entry/nim_class_macro
+- https://flat-leon.hatenablog.com/entry/nim_macro_programming
--- nimacro/src/nimacro.nim
+++ nimacro/src/nimacro.nim
@@ -1,38 +1,52 @@
 import strformat, macros
 
-macro html(body:untyped):untyped =
-  for indexB1, b1 in body:
-    if b1.len() > 1:
-      for indexB2, b2 in b1:
-        if b2.len() > 1:
-          for indexB3, b3 in b2:
-            if b3.len() > 1:
-              for indexB4, b4 in b3:
-                if b4.len() > 1:
-                  for indexB5, b5 in b4:
-                    if b5.len() > 1:
-                      for indexB6, b6 in b5:
-                        echo fmt"          body[{repr(indexB1)}][{repr(indexB2)}][{repr(indexB3)}][{repr(indexB4)}][{repr(indexB5)}][{repr(indexB6)}]={repr(b6)}: {repr($b6.kind)}"
-                    else:
-                      echo fmt"        body[{repr(indexB1)}][{repr(indexB2)}][{repr(indexB3)}][{repr(indexB4)}][{repr(indexB5)}]={repr(b5)}: {repr($b5.kind)}"
-                else:
-                  echo fmt"      body[{repr(indexB1)}][{repr(indexB2)}][{repr(indexB3)}][{repr(indexB4)}]={repr(b4)}: {repr($b4.kind)}"
-            else:
-              echo fmt"    body[{repr(indexB1)}][{repr(indexB2)}][{repr(indexB3)}]={repr(b3)}: {repr($b3.kind)}"
-        else:
-          echo fmt"  body[{repr(indexB1)}][{repr(indexB2)}]={repr(b2)}: {repr($b2.kind)}"
-    else:
-      echo fmt"body[{repr(indexB1)}]={repr(b1)}: {repr($b1.kind)}"
-  echo ""
+proc exportHtml(n: NimNode): string =
+  result = ""
+  if n.len() > 0:
+    var closeTag = ""
+    for child in n:
+      result.add(exportHtml(child))
+      # 閉じタグを覚えておく（空タグの場合は閉じない）
+      if n.len() > 1 and child.len() == 0 and child.kind == nnkIdent:
+        closeTag = fmt"</{repr(child)}>"
+    # タグを閉じる
+    if closeTag != "":
+      result.add(closeTag)
+  else:
+    case n.kind
+    of nnkIdent:
+      result.add(fmt"<{repr(n)}>")
+    of nnkStrLit:
+      result.add($n)
+    else: discard
+
+macro html(body: untyped): untyped =
+  var buff = "<html>"
+  buff.add(exportHtml(body))
+  buff.add("</html>")
+  parseStmt(fmt"""
+let src = "{buff}"
+echo(src)
+""")
 
 when isMainModule:
   html:
-    h1("HTMLのテスト")
-    main:
-      section:
-        h2("セクション1")
-        p("ほげほげ")
-      section:
-        h2("セクション2")
-        p("ふがふが")
-        p("ふごふご")
+    head:
+      title "hoge"
+    body:
+      h1 "HTMLのテスト"
+      main:
+        section:
+          h2 "セクション1"
+          p "ほげほげ"
+          p:
+            "ほげほげほげほげ"
+            span "01234567"
+            "ふがふが"
+        section:
+          h2 "セクション2"
+          p "ふがふが"
+          p:
+            "ふごふご"
+            br()
+            "fufefefe"
