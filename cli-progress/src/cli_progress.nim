ee525b9cb0210fd83f004624b92264c4419f9ad5
myamyu <myamyu@gmail.com>
2022-01-25 17:18:19 +0000

    青にした

--- cli-progress/src/cli_progress.nim
+++ cli-progress/src/cli_progress.nim
@@ -3,12 +3,12 @@ import os, strutils
 proc main() =
   stdout.write("\e[2J\e[0f") # 画面全消し→カーソルをリセット
   stdout.write("\e[31mHello, World!\e[m\n")
-  stdout.write("\e[42m\e[4mHello, World!\e[m\n")
+  stdout.write("\e[41m\e[4mHello, World!\e[m\n")
   stdout.write("\e[2mHello, \e[100mWorld!\e[m\n")
   stdout.write("\n")
   # プログレスバーを表示
   for i in 0..100:
-    stdout.write("|\e[42m$1\e[100m$2\e[m| $3%\r" % [
+    stdout.write("|\e[44m$1\e[100m$2\e[m| $3%\r" % [
       " ".repeat(i),
       " ".repeat(100 - i),
       $i,
